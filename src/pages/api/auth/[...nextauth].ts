8e566b1ee00ce26c9ee613888b71e63ec62d5364
Mauricio <ciumauri@gmail.com>
2022-11-12 02:24:28 -0300

    Feat: Configured createUser faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -11,6 +14,32 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email },
+            }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    },
+  },
 }
 
 export default NextAuth(authOptions)
