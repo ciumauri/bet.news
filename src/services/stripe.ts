3ea0c42a0cacd53f143aa3bc0de772877136e2a8
Mauricio Oliveira <ciumauri@gmail.com>
2022-11-12 21:18:15 -0300

    Feat: Avoiding duplication in the stripe

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 // import { version } from '../../package.json'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2022-08-01',
   appInfo: {
     name: 'Betnews',
